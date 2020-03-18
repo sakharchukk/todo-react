ce4609110b4ee81712793b3736f69c2aef9ede66
Dmytro <sakharchukcoder@gmail.com>
2020-03-18 23:33:05 +0200

    Updates

---
+++ precache-manifest.84d984e93084eb7af240295cea57bc4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b46d8da14d9f283cf64cea17340afce",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "430dfe081f6a30affac4",
+    "url": "/todo-react/static/css/main.aa974139.chunk.css"
+  },
+  {
+    "revision": "1749bba1cbd578a71947",
+    "url": "/todo-react/static/js/2.04fbd503.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-react/static/js/2.04fbd503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430dfe081f6a30affac4",
+    "url": "/todo-react/static/js/main.c383b813.chunk.js"
+  },
+  {
+    "revision": "87844529946a5db7e73e",
+    "url": "/todo-react/static/js/runtime-main.855c13ad.js"
+  }
+]);
